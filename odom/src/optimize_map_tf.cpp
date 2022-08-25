9c36f89246a8339535f2cee0245182f13477411f
root <liunao123>
2022-08-25 10:07:26 +0800

    把优化后的轨迹也可以写到文件里

--- odom/src/optimize_map_tf.cpp
+++ odom/src/optimize_map_tf.cpp
@@ -31,12 +31,14 @@
 #include <pcl/point_types.h>
 #include <pcl/common/transforms.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/point_cloud.h>
 
 #include <pcl/visualization/cloud_viewer.h>
 
 using namespace std;
 using namespace Eigen;
 
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZI;
 
 void scan_dir_get_filename(string path, vector<string> &filenames)
 {
@@ -93,8 +95,8 @@ geometry_msgs::PoseStamped to_PoseStamped(std::vector<double> v8)
 
 int main(int argc, char **argv)
 {
-	// std::string work_dir = "/home/map/0805/";
-	std::string work_dir = "/home/map/0805_big_imu_noise_pose_graph_0_0/";
+	std::string work_dir = "/home/map/0805/";
+	// std::string work_dir = "/home/map/0805_big_imu_noise_pose_graph_0_0/";
 
 	if (argc > 1)
 	{
@@ -165,21 +167,22 @@ int main(int argc, char **argv)
 	// 这个读取的顺序是对的
 	scan_dir_get_filename(work_dir + "pcd_lidar/", pcd_file);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr map(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZI>::Ptr map(new pcl::PointCloud<pcl::PointXYZI>);
 
 	pcl::visualization::CloudViewer viewer("Cloud Viewer");
 
 	cout << "size of pcd : " << pcd_file.size() << endl;
 	cout << "size of lio_pose : " << lio_pose.size() << endl;
 
+	ofstream lio_optimized_path_file(work_dir + "lio_optimized_path.txt", ios::out);
 	int cnts_ = 0;
 
-	for (int i = 0; i < pcd_file.size(); i += 10 )
-	// for (int i = 154; i < 157; i += 1 )
+	for (int i = 0; i < pcd_file.size(); i += 1)
+	// for (int i = 154; i < 1570; i += 1 )
 	{
 		// 跳过中间没有回环的部分，直接看回环部分的点云
-		if(i == 1000 )
-		    i = 6000;
+		// if(i == 3000 )
+		//     i = pcd_file.size() - 2000;
 
 		auto pos_last_g = pcd_file[i].find_last_of("/") + 1;
 		auto pos_last_d = pcd_file[i].find_last_of(".") + 1;
@@ -197,6 +200,20 @@ int main(int argc, char **argv)
 		if (pcd_time < start_time || pcd_time > end_time)
 		{
 			cout << "this pcd time is out the loop path time : " << pcd_file[i].substr(pos_last_g, pos_last_d - pos_last_g - 1) << endl;
+
+			lio_optimized_path_file.setf(ios::fixed, ios::floatfield);
+			lio_optimized_path_file.precision(10);
+			lio_optimized_path_file << pose_time << " ";
+			lio_optimized_path_file.precision(5);
+
+			lio_optimized_path_file
+				<< lio_pose[i].pose.position.x << " "
+				<< lio_pose[i].pose.position.y << " "
+				<< lio_pose[i].pose.position.z << " "
+				<< lio_pose[i].pose.orientation.w << " "
+				<< lio_pose[i].pose.orientation.x << " "
+				<< lio_pose[i].pose.orientation.y << " "
+				<< lio_pose[i].pose.orientation.z << endl;
 			continue;
 		}
 		else
@@ -228,66 +245,70 @@ int main(int argc, char **argv)
 
 					Eigen::Vector3d now_pcd_tans = start_pos + (end_pos - start_pos) * (dt_before / dt);
 
-					auto diff_posi = (end_pos - start_pos).transpose();
+					/*
+										double t = 180.0 / 3.1415926;
+										double roll, pitch, yaw;					  //定义存储r\p\y的容器
+										// 对旋转插值  用ros的 tf 工具
+										tf::Quaternion start_ori;
+										tf::quaternionMsgToTF(loop_pose[j].pose.orientation, start_ori);
+										start_ori.normalize();
+										tf::Matrix3x3(start_ori).getRPY(roll, pitch, yaw); //进行转换
+										// printf("start_ori tf eulerAngle: roll=%f ,pitch=%f ,yaw=%f .\n", roll * t, pitch * t, yaw * t);
 
-					double t = 180.0 / 3.1415926;
-					double roll, pitch, yaw;					  //定义存储r\p\y的容器
-					// 对旋转插值  用ros的 tf 工具
-					tf::Quaternion start_ori;
-					tf::quaternionMsgToTF(loop_pose[j].pose.orientation, start_ori);
-					start_ori.normalize();
-					tf::Matrix3x3(start_ori).getRPY(roll, pitch, yaw); //进行转换
-					// printf("start_ori tf eulerAngle: roll=%f ,pitch=%f ,yaw=%f .\n", roll * t, pitch * t, yaw * t);
+										tf::Quaternion end_ori;
+										tf::quaternionMsgToTF(loop_pose[j + 1].pose.orientation, end_ori);
+										end_ori.normalize();
 
-					tf::Quaternion end_ori;
-					tf::quaternionMsgToTF(loop_pose[j + 1].pose.orientation, end_ori);
-					end_ori.normalize();
-					
-					tf::Quaternion now_rat = start_ori.slerp(end_ori, (dt_before / dt));
-					end_ori.normalize();
-					tf::Matrix3x3(now_rat).getRPY(roll, pitch, yaw); //进行转换
-					// printf("now_rat tf eulerAngle:   roll=%f ,pitch=%f ,yaw=%f .\n", roll * t, pitch * t, yaw * t);
+										tf::Quaternion now_rat = start_ori.slerp(end_ori, (dt_before / dt));
+										now_rat.normalize();
+										tf::Matrix3x3(now_rat).getRPY(roll, pitch, yaw); //进行转换
 
-					// tf::Matrix3x3(end_ori).getRPY(roll, pitch, yaw); //进行转换
-					// printf("end_ori tf eulerAngle:   roll=%f ,pitch=%f ,yaw=%f .\n\n", roll * t, pitch * t, yaw * t);
+										printf("now_rat tf eulerAngle:   roll=%f ,pitch=%f ,yaw=%f .\n", roll * t, pitch * t, yaw * t);
 
+										tf::Matrix3x3(end_ori).getRPY(roll, pitch, yaw); //进行转换
+										printf("end_ori tf eulerAngle:   roll=%f ,pitch=%f ,yaw=%f .\n\n", roll * t, pitch * t, yaw * t);
+
+					*/
 
 					// 对平移插值 只用 了  Z 方向上的值
-					// now_pcd_tans(0) = 	lio_pose[i].pose.position.x ;  // ( now_pcd_tans(0) + lio_pose[i].pose.position.x ) / 2;
-					// now_pcd_tans(1) = 	lio_pose[i].pose.position.y ;  // ( now_pcd_tans(1) + lio_pose[i].pose.position.y ) / 2;
-					// now_pcd_tans(2) = 	lio_pose[i].pose.position.z;
-                    
-					// Eigen::Vector3d now_pcd_tans_next(lio_pose[i+1].pose.position.x, lio_pose[i+1].pose.position.y, lio_pose[i+1].pose.position.z);
-
-					// if(( now_pcd_tans_next - now_pcd_tans).norm() > 2)
-					// {
-
-					// 	PCL_ERROR("eeeeeeeeeeee %d ", i);
-
-					// 	while (1)
-					// 	{
-					// 	}
-
-					// 	break;
-					// }
+					now_pcd_tans(0) = lio_pose[i].pose.position.x; // ( now_pcd_tans(0) + lio_pose[i].pose.position.x ) / 2;
+					now_pcd_tans(1) = lio_pose[i].pose.position.y; // ( now_pcd_tans(1) + lio_pose[i].pose.position.y ) / 2;
+																   // now_pcd_tans(2) = 	lio_pose[i].pose.position.z;
 
 					// Eigen::Quaterniond now_pcd_rot = Eigen::Quaterniond(now_rat.w(),
 					// 									  now_rat.x(),
 					// 									  now_rat.y(),
 					// 									  now_rat.z());
 
-				    Eigen::Quaterniond now_pcd_rot = Eigen::Quaterniond(lio_pose[i].pose.orientation.w,
-														  lio_pose[i].pose.orientation.x,
-														  lio_pose[i].pose.orientation.y,
-														  lio_pose[i].pose.orientation.z);
+					Eigen::Quaterniond now_pcd_rot = Eigen::Quaterniond(lio_pose[i].pose.orientation.w,
+																		lio_pose[i].pose.orientation.x,
+																		lio_pose[i].pose.orientation.y,
+																		lio_pose[i].pose.orientation.z);
 
+					lio_optimized_path_file.setf(ios::fixed, ios::floatfield);
+					lio_optimized_path_file.precision(10);
+					lio_optimized_path_file << pose_time << " ";
+					lio_optimized_path_file.precision(5);
 
+					lio_optimized_path_file
+						<< now_pcd_tans.x() << " "
+						<< now_pcd_tans.y() << " "
+						<< now_pcd_tans.z() << " "
+						<< now_pcd_rot.w() << " "
+						<< now_pcd_rot.x() << " "
+						<< now_pcd_rot.y() << " "
+						<< now_pcd_rot.z() << endl;
 
 					// 根据上述 旋转和平移构造对应的 变换矩阵，把点云 投影过去即可。
-					pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+					pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+					// pcl::PointCloud<pcl::PointXYZI>::Ptr cloudI(new pcl::PointCloud<pcl::PointXYZI>);
+
 					try
 					{
-						pcl::io::loadPCDFile<pcl::PointXYZ>(pcd_file[i], *cloud);
+						int r = pcl::io::loadPCDFile<pcl::PointXYZI>(pcd_file[i], *cloud);
+						// pcl::PCDReader reader;
+						// int r = reader.read<pcl::PointXYZI>(pcd_file[i], *cloud);
+						std::cout << r << '\n';
 					}
 					catch (const std::exception &e)
 					{
@@ -298,17 +319,17 @@ int main(int argc, char **argv)
 						break;
 					}
 
-					pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+					pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
 					transformed_cloud->points.resize(cloud->points.size());
 
 					// static const Eigen::Vector3d Lidar_offset_to_IMU_temp(0.05512, 0.02226, 0.0297); // Horizon r2live
 					// 官方 https://livox-wiki-cn.readthedocs.io/zh_CN/latest/introduction/Point_Cloud_Characteristics_and_Coordinate_System%20.html#id2
 					// static const Eigen::Vector3d Lidar_offset_to_IMU_temp(-0.0847, -0.0425, -0.0353); // -84.7，-42.5，-35.3 // one
-					// static const Eigen::Vector3d Lidar_offset_to_IMU_temp(0.0847, 0.0425, 0.0353); // -84.7，-42.5，-35.3 // two
+					static const Eigen::Vector3d Lidar_offset_to_IMU_temp(0.0847, 0.0425, 0.0353); // -84.7，-42.5，-35.3 // two
 					// static const Eigen::Vector3d Lidar_offset_to_IMU_temp(0.05512, 0.02226, 0.0297); // Horizon r2live three
 					//    static const Eigen::Vector3d Lidar_offset_to_IMU_temp(-0.05512, -0.02226, -0.0297); // Horizon r2live four
 					//    static const Eigen::Vector3d Lidar_offset_to_IMU_temp(0.0, 0.0, 0.0); //  zero
-					   static const Eigen::Vector3d Lidar_offset_to_IMU_temp(-0.0575, -0.0375, 0.0425); //  zero
+					//    static const Eigen::Vector3d Lidar_offset_to_IMU_temp(-0.0575, -0.0375, 0.0425); //  zero
 
 					// 构建变换矩阵
 					Eigen::Isometry3d TM = Eigen::Isometry3d::Identity();
@@ -316,34 +337,42 @@ int main(int argc, char **argv)
 					TM.rotate(now_pcd_rot.matrix());
 					// cout << TM.matrix() << endl;
 
-					for (int in=0; in < cloud->points.size() ; in++)
+					for (int in = 0; in < cloud->points.size(); in++)
 					{
-					    cloud->points[in].x += Lidar_offset_to_IMU_temp[0];
-					    cloud->points[in].y += Lidar_offset_to_IMU_temp[1];
-					    cloud->points[in].z += Lidar_offset_to_IMU_temp[2];
+						if (cloud->points[in].x < 0)
+						{
+							cloud->points.clear();
+							cout << "ERROR PCD <  x < 0 . -_- >. " << pcd_file[i] << endl;
+							break;
+						}
+						cloud->points[in].x += Lidar_offset_to_IMU_temp[0];
+						cloud->points[in].y += Lidar_offset_to_IMU_temp[1];
+						cloud->points[in].z += Lidar_offset_to_IMU_temp[2];
 					}
-					pcl::transformPointCloud(*cloud, *transformed_cloud, TM.matrix() );
+					pcl::transformPointCloud(*cloud, *transformed_cloud, TM.matrix());
 
 					(*map) += (*transformed_cloud);
-					viewer.showCloud( map );
+					viewer.showCloud(map);
 					boost::this_thread::sleep(boost::posix_time::microseconds(1));
-
 					break;
 				}
 			}
 		}
 	}
 
+	lio_optimized_path_file.close();
+
 	cout << "--cnts_ ----DONE----- " << cnts_ << endl;
 	cout << " START WRITE PCD . " << endl;
 
-	// return 0;
+	return 0;
 
 	while (!viewer.wasStopped())
-	{    }
+	{
+	}
 	/*	*/
 
-	pcl::VoxelGrid<pcl::PointXYZ> sor;
+	pcl::VoxelGrid<pcl::PointXYZI> sor;
 
 	sor.setInputCloud(map);
 	float resolution = 0.1f;
