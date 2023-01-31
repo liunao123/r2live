151773290417bd91f9301147ed36c5481f1d7ece
liunao123 <812807457@qq.com>
2023-01-31 10:42:51 +0800

    delete not need include header

--- odom/src/rotation_pcd_to_horizon_pcl.cpp
+++ odom/src/rotation_pcd_to_horizon_pcl.cpp
@@ -1,6 +1,5 @@
 #include <pcl/io/pcd_io.h>
 
-#include <pcl_ros/point_cloud.h>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
